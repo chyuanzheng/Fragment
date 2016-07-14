5d23668c00b91f73315a9f6b5fb19c9f62271ab8
cyz <chyuanzheng@126.com>
2016-07-14 15:10:59 +0800

    Initial commit

---
+++ fragment/src/main/java/com/mydream/fragment/MainActivity.java
@@ -0,0 +1,41 @@
+package com.mydream.fragment;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+import android.view.Window;
+import android.widget.RadioGroup;
+
+
+
+/**
+ * 主界面
+ */
+public class MainActivity extends Activity implements RadioGroup.OnCheckedChangeListener {
+    private FragmentManager fragmentManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity);
+
+        fragmentManager = getFragmentManager();
+        RadioGroup radioGroup = (RadioGroup) findViewById(R.id.rg_tab);
+        radioGroup.setOnCheckedChangeListener(this);
+
+        //default
+        findViewById(R.id.rb_attention).performClick();
+    }
+
+    @Override
+    public void onCheckedChanged(RadioGroup group, int checkedId) {
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        //实例化需要的fragment
+        Fragment fragment = FragmentFactory.getInstanceByIndex(checkedId);
+        transaction.replace(R.id.content, fragment);
+        transaction.commit();
+    }
+}
